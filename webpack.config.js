84775b66509a01ddeac84c7b5454245abd77047c
Andreas Roth <andreas.roth@esveo.com>
2021-03-21 14:35:01 +0100

    Fix prod config

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ config.module = {
         options: {
           plugins: [
             process.env.NODE_ENV !== "production" && "react-refresh/babel",
-          ],
+          ].filter(Boolean),
         },
       },
     },
